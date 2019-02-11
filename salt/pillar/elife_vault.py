621f87be5f68e811a2a25d74d7285a58cd3e6729
Giorgio Sironi <g.sironi@elifesciences.org>
2019-02-11 17:32:24 +0000

    Adjust logs verbosity

--- salt/pillar/elife_vault.py
+++ salt/pillar/elife_vault.py
@@ -3,19 +3,15 @@ import logging
 log = logging.getLogger(__name__)
 
 def ext_pillar(minion_id, pillar, path=None, env_key=None, vault_addr=None):
-    log.critical("pillar: %s", type(pillar))
-    log.critical("elife_vault: %s %s", path, vault_addr)
-    log.critical("grains project: %s", __grains__['project'])
     env = pillar
     for key in env_key:
         env = env[key]
-    log.critical("env: %s", env)
     vault_key = path.format(project=__grains__['project'], env=env)
-    log.critical("vault_key: %s", vault_key)
+    log.info("Reading vault_key: %s", vault_key)
     vault_value = __salt__['vault.read_secret'](vault_key)
-    log.critical("vault_value: %s", vault_value)
     vault_pillar = {}
     for pillar_path in vault_value['data']:
+        log.debug("Adding pillar: %s", pillar_path)
         pillar_branch = vault_pillar
         sections = pillar_path.split(".")
         key = sections[-1]
@@ -26,6 +22,5 @@ def ext_pillar(minion_id, pillar, path=None, env_key=None, vault_addr=None):
             pillar_branch = pillar_branch[section]
         value = vault_value['data'][pillar_path]
         pillar_branch[key] = value
-    log.critical("vault_pillar: %s", vault_pillar)
     return vault_pillar
 
