83891abdcfb334cabdb376f472f33efc24c5f44f
Giorgio Sironi <g.sironi@elifesciences.org>
2019-02-12 08:10:40 +0000

    Write documentation for elife_vault ext pillar module

--- salt/master-server/config/opt-salt-extension-modules-pillar-elife_vault.py
+++ salt/master-server/config/opt-salt-extension-modules-pillar-elife_vault.py
@@ -1,7 +1,31 @@
+'''
+eLife Vault Pillar Module
+
+This module allows pillar data to be from Hashicorp Vault.
+Below are noted extra configuration required for the pillar module, but the base
+configuration of Salt's own ``vault`` module must also be completed.
+After the base Vault configuration is created, add the configuration below to
+the ext_pillar section in the Salt master configuration.
+.. code-block:: yaml
+    ext_pillar:
+	- elife_vault:
+	     path: secret/data/projects/{project}/{env}
+	     env_key: ["elife", "env"]
+
+Each Vault key needs to have all the key-value pairs with the names you
+require. The dot ``.`` separates different nesting levels of the values:
+.. code-block:: bash
+    $ vault write secret/projects/elife-xpub/prod smtp.username=foo smtp.password=my_password
+The above will result in ``pillar.smtp`` being available as a dictionary with two keys,
+ which as for every external pillar will be merged with the rest of the pillars in the Salt master.
+
+'''
+
 import logging
 
 log = logging.getLogger(__name__)
 
+# TODO: remove vault_addr
 def ext_pillar(minion_id, pillar, path=None, env_key=None, vault_addr=None):
     env = pillar
     for key in env_key:
