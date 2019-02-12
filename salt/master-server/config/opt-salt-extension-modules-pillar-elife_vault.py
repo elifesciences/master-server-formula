6624b9f9eaf1e140cf3a8c405c8dd3ab1f2d24d5
Giorgio Sironi <g.sironi@elifesciences.org>
2019-02-12 08:17:25 +0000

    Insert comments into ext_pillar

--- salt/master-server/config/opt-salt-extension-modules-pillar-elife_vault.py
+++ salt/master-server/config/opt-salt-extension-modules-pillar-elife_vault.py
@@ -42,8 +42,13 @@ def ext_pillar(minion_id, pillar, path=None, env_key=None):
     log.info("Reading vault_key: %s", vault_key)
     vault_value = __salt__['vault.read_secret'](vault_key)
     vault_pillar = {}
+
+    # expand the empty vault_pillar with dictionaries
+    # {} => {'elife_xpub': {'smtp': {'username': 'foo'}}}
     for pillar_path in vault_value['data']:
         log.debug("Adding pillar: %s", pillar_path)
+        # pillar_branch is a pointer within vault_pillar, 
+        # initially pointing to the 'root'
         pillar_branch = vault_pillar
         sections = pillar_path.split(".")
         key = sections[-1]
@@ -51,6 +56,7 @@ def ext_pillar(minion_id, pillar, path=None, env_key=None):
         for section in sections:
             if section not in pillar_branch:
                 pillar_branch[section] = {}
+            # updates pointer
             pillar_branch = pillar_branch[section]
         value = vault_value['data'][pillar_path]
         pillar_branch[key] = value
